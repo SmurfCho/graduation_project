7dbfaa9732adb5699a37970aed8cb456aa27566e
WingCho <yingwoo@163.com>
2018-03-29 11:00:52 +0800

    layout adjust

--- slides/src/js/components/pc_editor.js
+++ slides/src/js/components/pc_editor.js
@@ -22,7 +22,7 @@ render(){
         <div>
         <PCHeader/>
             <Layout>
-              <Sider width="100" style={{ overflow: 'auto', height: '100vh', position:'fixed', left: 0 }}>
+              <Sider width="80" style={{ overflow: 'auto', height: '100vh', position:'fixed', left: 0 }}>
                     <Button id="tool-btn1" title="预览">
                       <Icon class="tool-icon" type="scan" />
                     </Button>
@@ -30,7 +30,7 @@ render(){
                       <Icon class="tool-icon" type="rollback" />
                     </Button>
                     <Button id="tool-btn3" title="保存">
-                      <Icon class="tool-icon" type="upload" />
+                      <Icon class="tool-icon" type="save" />
                     </Button>
                     <Button id="tool-btn4" title="播放">
                       <Icon class="tool-icon" type="caret-right" />
@@ -42,8 +42,20 @@ render(){
                       <Icon class="tool-icon" type="setting" />
                     </Button>
               </Sider>
-              <Sider width="150" style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 100 }}>
+              <Sider width="80" style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 80 }}>
+                <Button id="text" title="添加文本">
+                  <Icon class="tool-icon" type="file-text" />
+                </Button>
+                <Button id="images" title="添加图片">
+                    <Icon class="tool-icon" type="picture" />
+                </Button>
+                <Button id="video" title="添加视频">
+                    <Icon class="tool-icon" type="video-camera" />
+                </Button>
               </Sider>
+              <Content style={{position:'fixed', left:160,width:100%,height:100%,margin: '24px 16px', padding: 24, background: '#eee', minHeight: 280 }}>
+                <section class="page"></section>
+              </Content>
             </Layout>
             </div>
 
