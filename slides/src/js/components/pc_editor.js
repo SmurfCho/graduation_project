9740c8f97554d1d5049b1c01637f55c7af61531e
SmurfCho <yinghua_c@Yahoo.com>
2018-04-04 10:29:02 +0800

    conflict manage

--- slides/src/js/components/pc_editor.js
+++ slides/src/js/components/pc_editor.js
@@ -76,23 +76,14 @@ render(){
         <div>
         <PCHeader/>
             <Layout>
-<<<<<<< HEAD
               <Sider class="sider" width="80" style={{ overflow: 'scroll', height: '100vh', position:'fixed', left: 0 }}>
                     <Button htmlType="button" id="tool-btn1" title="预览">
-=======
-              <Sider width="80" style={{ overflow: 'auto', height: '100vh', position:'fixed', left: 0 }}>
-                    <Button id="tool-btn1" title="预览">
->>>>>>> 7dbfaa9732adb5699a37970aed8cb456aa27566e
                       <Icon class="tool-icon" type="scan" />
                     </Button>
                     <Button htmlType="button" id="tool-btn2" title="撤销">
                       <Icon class="tool-icon" type="rollback" />
                     </Button>
-<<<<<<< HEAD
                     <Button htmlType="button" id="tool-btn3" title="保存">
-=======
-                    <Button id="tool-btn3" title="保存">
->>>>>>> 7dbfaa9732adb5699a37970aed8cb456aa27566e
                       <Icon class="tool-icon" type="save" />
                     </Button>
                     <Button htmlType="button" id="tool-btn4" title="播放">
@@ -105,7 +96,6 @@ render(){
                       <Icon class="tool-icon" type="setting" />
                     </Button>
               </Sider>
-<<<<<<< HEAD
               <Sider class="sider" id="func-menu" width="80" style={{ display:this.state.originSider, overflow: 'scroll', height: '100vh', position: 'fixed', left: 80 }}>
                 <Button htmlType="button" id="text" title="添加文本"  onClick={this.siderDisplay.bind(this)}>
                   <Icon class="tool-icon" type="file-text" />
@@ -140,21 +130,6 @@ render(){
                   <Button class="add-slides" htmlType="button" onClick={this.addRightSlides.bind(this)} style={{position:'fixed',height:'50px',width:'50px',border:'none',top:'50%',left:'95%',fontSize:50,textarea:'center',background:'none'}}><Icon type="plus-circle-o" /></Button>
                   <Button class="add-slides" htmlType="button" onClick={this.addDownSlides.bind(this)} style={{position:'fixed',height:'50px',width:'50px',border:'none',top:'90%',left:'50%',fontSize:50,textarea:'center',background:'none'}}><Icon type="plus-circle-o" /></Button>
                 </article>
-=======
-              <Sider width="80" style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 80 }}>
-                <Button id="text" title="添加文本">
-                  <Icon class="tool-icon" type="file-text" />
-                </Button>
-                <Button id="images" title="添加图片">
-                    <Icon class="tool-icon" type="picture" />
-                </Button>
-                <Button id="video" title="添加视频">
-                    <Icon class="tool-icon" type="video-camera" />
-                </Button>
-              </Sider>
-              <Content style={{position:'fixed', left:160,width:100%,height:100%,margin: '24px 16px', padding: 24, background: '#eee', minHeight: 280 }}>
-                <section class="page"></section>
->>>>>>> 7dbfaa9732adb5699a37970aed8cb456aa27566e
               </Content>
             </Layout>
             </div>
